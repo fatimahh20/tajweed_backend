ab8de213f119860ab09c003f8d536e8793a23946
fatimahh20 <muteeahfatima@gmail.com>
2026-03-06 09:44:18 +0500

    Update app.py

--- app.py
+++ app.py
@@ -62,5 +62,5 @@ async def analyze_tajweed(word_id: str = Query(...), file: UploadFile = File(...
 
 
     if __name__ == "__main__":
-     port = int(os.environ.get("PORT", 8000))
+     port = int(os.environ.get("PORT", 10000))
      uvicorn.run("main:app", host="0.0.0.0", port=port)   
